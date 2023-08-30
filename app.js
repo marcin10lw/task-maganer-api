6955a4f778b006e26600df256d9f5def7e3289b8
marcin10lw <marcin10lw@wp.pl>
2023-08-30 09:05:08 +0200

    created basic app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+app.use(express.json());
+app.use("/api/v1/tasks", tasks);
+
+port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
